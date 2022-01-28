5b9cd1cd0a10acf48f512cf9bc3bf15d7b4d84d4
Lauma Alves <laumasantos@gmail.com>
2022-01-28 18:59:24 -0300

    atualização do projeto

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -26,7 +26,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -43,9 +43,19 @@ export function TrasactionsProvider({ children }: TransactionsProviderProps) {
         .then(response => setTransactions(response.data.transactions))
     }, []);
 
-    function createTransaction(transaction: TransactionInput) {
+    async function createTransaction(transactionInput: TransactionInput) {
 
-        api.post('/transactions', transaction)
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt : new Date(),
+        })
+        
+        const { transaction } = response.data;
+
+        setTransactions([
+            ...transactions,
+            transaction,
+        ]);
     }
 
     return (
